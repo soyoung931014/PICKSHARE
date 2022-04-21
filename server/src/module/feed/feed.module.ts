77bfce9970d7c259fe280476caa22240b1458414
Boyoung <boyoung.y589@gmail.com>
2022-04-21 17:39:41 +0900

    ADD: controller.ts and service.ts files

--- server/src/module/feed/feed.module.ts
+++ server/src/module/feed/feed.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { FeedService } from './feed.service';
+import { FeedController } from './feed.controller';
 
-@Module({})
+@Module({
+  providers: [FeedService],
+  controllers: [FeedController]
+})
 export class FeedModule {}
--- server/src/module/follow/follow.module.ts
+++ server/src/module/follow/follow.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { FollowService } from './follow.service';
+import { FollowController } from './follow.controller';
 
-@Module({})
+@Module({
+  providers: [FollowService],
+  controllers: [FollowController]
+})
 export class FollowModule {}
