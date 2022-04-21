4f8de023fd36fe31a35cf65e3ac1129cc3dc24e7
park_so_young <soyoung931014@gmail.com>
2022-04-21 14:22:43 +0900

    ADD: Initial Setting

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const a: String = 'asd'
+
+
+
+  return (
+    <div className="App">
+      hello world
+    </div>
+  );
+}
+
+export default App;
