cdbceb00ecf2b3f96c0563f0d116a4a9c21beee6
park_so_young <soyoung931014@gmail.com>
2022-04-27 12:43:45 +0900

    ADD: Login_일반

--- server/src/module/user/user.service.ts
+++ server/src/module/user/user.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { LoginDto } from './dto/login-user.dto';
 import { SignUpDto } from './dto/singup-user.dto';
 import { UserRepository } from './user.repository';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserService {
@@ -19,10 +20,10 @@ export class UserService {
         console.log(loginDto, 'loginDto')
         const { email, password } = loginDto;
 
-        const user = await this.userRepository.findOne({ email, password })
+        const user = await this.userRepository.findOne({ email })
 
-        if (user) {
-            return { message: 'login success', data: `${loginDto}`, statusCode: 200 }
+        if (user && await bcrypt.compare(password, user.password)) {
+            return { message: 'login success', data: `${user.email}`, statusCode: 200 }
         } else {
             throw new UnauthorizedException('invalid user information')
         }
