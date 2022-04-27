93146e1ec0c3db429211098f0b36f4f939247020
park_so_young <soyoung931014@gmail.com>
2022-04-27 11:35:48 +0900

    ADD: SignUp기능

--- server/src/module/user/user.controller.ts
+++ server/src/module/user/user.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { SignUpDto } from './dto/singup-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService) { }
+
+    //중복검사(emailcheck)
+
+    //중복검사(nickcheck)
+
+    // 회원가입
+    @Post('/signup')
+    signup(@Body() signUpDto: SignUpDto): Promise<{ message: string, statusCode: number }> {
+        return this.userService.signUp(signUpDto)
+    }
+
+
+
+}
--- server/src/module/user/user.service.ts
+++ server/src/module/user/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { SignUpDto } from './dto/singup-user.dto';
+import { UserRepository } from './user.repository';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(UserRepository)
+        private userRepository: UserRepository
+    ) { }
+
+    async signUp(signUpDto: SignUpDto): Promise<{ message: string, statusCode: number }> {
+        return this.userRepository.createUser(signUpDto)
+    }
+}
