a57006c892af705ce326b6a23f592d61fc4cd442
park_so_young <soyoung931014@gmail.com>
2022-04-27 15:59:03 +0900

    ADD: 토큰생성,일반로그인완성

--- server/src/module/user/user.service.ts
+++ server/src/module/user/user.service.ts
@@ -4,26 +4,35 @@ import { LoginDto } from './dto/login-user.dto';
 import { SignUpDto } from './dto/singup-user.dto';
 import { UserRepository } from './user.repository';
 import * as bcrypt from 'bcryptjs';
+import { TokenService } from '../token/token.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserService {
     constructor(
         @InjectRepository(UserRepository)
-        private userRepository: UserRepository
+        private userRepository: UserRepository,
+        private token: JwtService
     ) { }
 
     async signUp(signUpDto: SignUpDto): Promise<{ message: string, statusCode: number }> {
         return this.userRepository.createUser(signUpDto)
     }
 
-    async login(loginDto: LoginDto): Promise<{ message: string, data: string, statusCode: number }> {
+    async login(loginDto: LoginDto): Promise<{ message: string, data: Object, statusCode: number }> {
         console.log(loginDto, 'loginDto')
         const { email, password } = loginDto;
 
         const user = await this.userRepository.findOne({ email })
+        // console.log('user', user)
 
         if (user && await bcrypt.compare(password, user.password)) {
-            return { message: 'login success', data: `${user.email}`, statusCode: 200 }
+            const { id, email, nickname, userImage, statusMessage, loginMethod, created_at, updated_at } = user;
+            const accessToken = await this.token.sign({ id, email, nickname, userImage, statusMessage, loginMethod, created_at, updated_at });
+
+            return {
+                message: 'login success', data: { accessToken, loginMethod }, statusCode: 200
+            }
         } else {
             throw new UnauthorizedException('invalid user information')
         }
