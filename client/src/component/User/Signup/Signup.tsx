f96ed9f8fecef47d9b727f80ccd3d41c558a46c6
park_so_young <soyoung931014@gmail.com>
2022-05-04 15:40:21 +0900

    ADD: Signup

--- client/src/component/User/Signup/Signup.tsx
+++ client/src/component/User/Signup/Signup.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Container = styled.div``;
+const SignupBox = styled.div`
+  width: 40rem;
+  height: 100rem;
+  border-color: aliceblue;
+  background-color: teal;
+`;
 function Signup() {
-  return <div>hi</div>;
+  return (
+    <Container>
+      <SignupBox>
+        <input />
+      </SignupBox>
+    </Container>
+  );
 }
 
 export default Signup;
