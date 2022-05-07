b64871e276d887564d7769afa6a5bc17ba8f4812
park_so_young <soyoung931014@gmail.com>
2022-05-07 23:08:33 +0900

    ADD: 회원가입- 이메일, 닉네임 중복체크 #22

--- server/src/module/user/user.service.ts
+++ server/src/module/user/user.service.ts
@@ -9,32 +9,73 @@ import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectRepository(UserRepository)
-        private userRepository: UserRepository,
-        private token: JwtService
-    ) { }
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+    private token: JwtService,
+  ) {}
 
-    async signUp(signUpDto: SignUpDto): Promise<{ message: string, statusCode: number }> {
-        return this.userRepository.createUser(signUpDto)
+  async getEmailCheck(id: string): Promise<object> {
+    const emailcheck = await this.userRepository.findOne({ email: id });
+    if (emailcheck) {
+      return { message: '이미 사용중인 이메일입니다.' };
+    } else {
+      return { message: '사용할 수 있는 이메일입니다.' };
     }
+  }
 
-    async login(loginDto: LoginDto): Promise<{ message: string, data: object, statusCode: number }> {
-        console.log(loginDto, 'loginDto')
-        const { email, password } = loginDto;
-
-        const user = await this.userRepository.findOne({ email })
-        // console.log('user', user)
-
-        if (user && await bcrypt.compare(password, user.password)) {
-            const { id, email, nickname, userImage, statusMessage, loginMethod, created_at, updated_at } = user;
-            const accessToken = await this.token.sign({ id, email, nickname, userImage, statusMessage, loginMethod, created_at, updated_at });
-
-            return {
-                message: 'login success', data: { accessToken, loginMethod }, statusCode: 200
-            }
-        } else {
-            throw new UnauthorizedException('invalid user information')
-        }
+  async getNicknameCheck(id: string): Promise<object> {
+    const nicknamecheck = await this.userRepository.findOne({ nickname: id });
+    if (nicknamecheck) {
+      return { message: '이미 사용중인 닉네임입니다.' };
+    } else {
+      return { message: '사용할 수 있는 닉네임입니다.' };
     }
+  }
+  async signUp(
+    signUpDto: SignUpDto,
+  ): Promise<{ message: string; statusCode: number }> {
+    return this.userRepository.createUser(signUpDto);
+  }
+
+  async login(
+    loginDto: LoginDto,
+  ): Promise<{ message: string; data: object; statusCode: number }> {
+    console.log(loginDto, 'loginDto');
+    const { email, password } = loginDto;
+
+    const user = await this.userRepository.findOne({ email });
+    // console.log('user', user)
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const {
+        id,
+        email,
+        nickname,
+        userImage,
+        statusMessage,
+        loginMethod,
+        created_at,
+        updated_at,
+      } = user;
+      const accessToken = await this.token.sign({
+        id,
+        email,
+        nickname,
+        userImage,
+        statusMessage,
+        loginMethod,
+        created_at,
+        updated_at,
+      });
+
+      return {
+        message: 'login success',
+        data: { accessToken, loginMethod },
+        statusCode: 200,
+      };
+    } else {
+      throw new UnauthorizedException('invalid user information');
+    }
+  }
 }
