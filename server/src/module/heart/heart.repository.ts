1737b16bbb7587589b3525bcd2cf61e313ebe0a0
Boyoung <boyoung.y589@gmail.com>
2022-05-09 17:18:10 +0900

    DELETE: console.log

--- server/src/module/heart/heart.repository.ts
+++ server/src/module/heart/heart.repository.ts
@@ -5,20 +5,11 @@ import { Heart } from "./heart.entity";
 @EntityRepository(Heart)
 export class HeartRepository extends Repository<Heart> {
 	async postHeart( user: User, board_id: number): Promise<Heart[]> {
-		console.log(user.id, 'user-HeartRepository')
-		console.log(board_id, 'board-HeartRepository')
-
-		// const numBoard_id = Number(board_id) 
-
 		const hearts = {
 			'user_id': user.id, 
-			// 'board_id': numBoard_id
 			'board_id': board_id
 		};
 		await this.save(hearts);
-		
-		console.log(hearts, 'hearts-HeartRepository')
-		
 		return this.find({
 			where: {
 				board_id 
