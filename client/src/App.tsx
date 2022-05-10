c595e6200fafbe234234e1206db182b173f5f710
Jaekomplett <wanzekim@gmail.com>
2022-05-10 17:23:22 +0900

    ADD: Client Routers & Pages setting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Signup from './component/User/Signup/Signup';
-import './App.css';
+import GlobalStyles from './GlobalStyles';
+import DiaryPage from './pages/DiaryPage';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      hello world
-      <Signup />
-    </div>
+    <BrowserRouter>
+      <GlobalStyles />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/diary" element={<DiaryPage />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/LandingPage.tsx
@@ -0,0 +1,15 @@
+import Footer from '../component/Footer/Footer';
+import Landing from '../component/Landing/Landing';
+import Nav from '../component/Nav/Nav';
+
+const LandingPage = () => {
+  return (
+    <>
+      <Nav />
+      <Landing />
+      <Footer />
+    </>
+  );
+};
+
+export default LandingPage;
