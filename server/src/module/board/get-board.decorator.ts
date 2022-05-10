f3c500812d34f3df0987ba2728314c5c9bda5937
Boyoung <boyoung.y589@gmail.com>
2022-05-10 17:19:54 +0900

    ADD: description of fix-need point

--- server/src/module/board/get-board.decorator.ts
+++
@@ -1,9 +0,0 @@
-// import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-// import { Board } from "./board.entity";
-
-// export const GetBoard = createParamDecorator(
-//     (data: unknown, ctx: ExecutionContext): Board => {
-//         const req = ctx.switchToHttp().getRequest();
-//         return req.board;
-//     },
-// );
