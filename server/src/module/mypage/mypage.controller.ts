808103b1065938d8fbe5f7866de7c33f56953463
park_so_young <soyoung931014@gmail.com>
2022-05-12 20:59:10 +0900

    ADD: 회원탈퇴, 회원정보수정 기능구현 #52

--- server/src/module/mypage/mypage.controller.ts
+++ server/src/module/mypage/mypage.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Patch,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { PasswordCheckDto } from './dto/passwordCheck.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { MypageService } from './mypage.service';
 
 @Controller('mypage')
-export class MypageController {}
+@UseGuards(AuthGuard())
+export class MypageController {
+  constructor(private mypage: MypageService) {}
+  // 회원정보 수정
+  @Patch('/update')
+  updateUser(
+    @Req() req: any,
+    @Body() updateUser: UpdateUserDto,
+  ): Promise<object> {
+    //console.log(req, 'req');
+    const { user } = req;
+    //console.log(user);
+    return this.mypage.updateUserInfo(updateUser, user);
+  }
+
+  //회원탈퇴
+  @Delete('/withdrawl')
+  deleteUser(
+    @Req() req: any,
+    @Body() passwordDto: PasswordCheckDto,
+  ): Promise<object> {
+    const { user } = req;
+    // console.log(user, 'user');
+    return this.mypage.removeUserInfo(user, passwordDto);
+  }
+}
--- server/src/module/mypage/mypage.service.ts
+++ server/src/module/mypage/mypage.service.ts
@@ -1,4 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from '../user/user.repository';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PasswordCheckDto } from './dto/passwordCheck.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class MypageService {}
+export class MypageService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {}
+
+  // 회원정보 수정
+  async updateUserInfo(updateUser: UpdateUserDto, user: any): Promise<object> {
+    const userInfo = await this.userRepository.findOne({ email: user.email });
+    if (userInfo) {
+      const updateUserInfo: object = { ...userInfo, ...updateUser };
+      await this.userRepository.save(updateUserInfo);
+      return { message: 'update success', statusCode: 200 };
+    } else {
+      return { message: 'update fail', statusCode: 400 };
+    }
+  }
+
+  // 회원 탈퇴
+  async removeUserInfo(
+    user: any,
+    passwordDto: PasswordCheckDto,
+  ): Promise<object> {
+    const userInfo = await this.userRepository.findOne({ email: user.email });
+    console.log(userInfo);
+    /* 만약 토큰 문제라면, userInfo가 없다면 useGuard에서 {
+    "statusCode": 401,
+    "message": "Unauthorized"
+    }에러 처리남 */
+    if (
+      userInfo &&
+      (await bcrypt.compare(passwordDto.password, userInfo.password))
+    ) {
+      await this.userRepository.remove(userInfo);
+      return { message: 'withdrawal success', statusCode: 200 };
+    } else {
+      return { message: 'withdrawal fail', statusCode: 400 }; // 올바르지 않은 비밀번호입니다.
+    }
+  }
+}
