f3fbfb511b0bc980a8e732bffd839b33534de89e
park_so_young <soyoung931014@gmail.com>
2022-05-25 18:33:43 +0900

    ADD: REDUX PERSIST #78

--- client/src/redux/index.tsx
+++ client/src/redux/index.tsx
@@ -1,10 +1,17 @@
 // comobineReducers: 리듀서가 2개 이상일 경우 combineReducer메소드에 객체형식으로
 // reducer를 넣는다.
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from './reducers/userReducer/userReducer';
 
 const rootReducer = combineReducers({
   userInfo: userReducer,
 });
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['userInfo'],
+};
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
