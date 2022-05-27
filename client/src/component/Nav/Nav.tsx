734aca8567bf8db1c55e396b4d715519568e8c0b
park_so_young <soyoung931014@gmail.com>
2022-05-28 02:34:55 +0900

    FIX Link 에서  NavLink 변경 및 프로필 사진 가져오기 #86

--- client/src/component/Nav/Nav.tsx
+++ client/src/component/Nav/Nav.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
@@ -6,11 +7,11 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 import { connect, useSelector } from 'react-redux';
 import { NavLink, useNavigate } from 'react-router-dom';
-
 import styled from 'styled-components';
 import { BiHome } from 'react-icons/bi';
-import { Link } from 'react-router-dom';
 import { deleteUserInfo } from '../../redux/actions';
+const AWS = require('aws-sdk/dist/aws-sdk-react-native');
+
 const Wrapper = styled.section`
   //border: solid green 2px;
   width: 100%;
@@ -35,7 +36,6 @@ const Logo = styled.section`
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   font-size: 2rem;
-
   :hover {
     cursor: pointer;
   }
@@ -48,23 +48,47 @@ const NavList = styled.section`
   justify-content: flex-end;
   align-items: center;
 `;
-const Div = styled.div<{ Home?: any }>`
+const Div = styled.div<{ Text?: any }>`
   //border: solid blue 2px;
-  margin: 1rem;
+  margin: 0.7rem;
   font-size: 1rem;
-  padding: 0.5rem;
+  padding: 0.6rem;
   :hover {
     cursor: pointer;
     background: #fee9f7;
     border-radius: 10px;
   }
+  padding-top: ${(props) => (props.Text ? '1rem' : null)};
+  padding-bottom: ${(props) => (props.Text ? '1rem' : null)};
+`;
+
+const Img = styled.img`
+  height: 3.5vh;
+  width: 1.8vw;
+  //border: solid red 2px;
+  border-radius: 50%;
+  padding: 2px;
+  margin-right: 0.5rem;
+`;
+const Info = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Nav = (props: any) => {
-  const { isLogin, accessToken, userInfo } = useSelector(
+  const { isLogin, accessToken, userInfo, userImage } = useSelector(
     (userReducer: any) => userReducer.userInfo
   );
   const navigate = useNavigate();
+  console.log(userInfo.userImage, 'userImage');
+
+  AWS.config.update({
+    region: 'us-east-1', // congito IdentityPoolId 리전을 문자열로 입력하기. 아래 확인 (Ex. "ap-northeast-2")
+    credentials: new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: 'us-east-1:156ae187-f9d1-49d9-86f7-ad7f49675cbd', // cognito 인증 풀에서 받아온 키를 문자열로 입력하기. (Ex. "ap-northeast-2...")
+    }),
+  });
 
   return (
     <>
@@ -73,84 +97,89 @@ const Nav = (props: any) => {
         {isLogin === true ? (
           <>
             <NavList>
-              <Div Home>
-                <Link
+              <Div>
+                <NavLink
                   to="/mainfeed"
                   style={{
                     textDecoration: 'none',
-                    color: 'black',
+                    color: '#3d3c3c',
                     fontSize: '1.7rem',
                   }}
                 >
                   <BiHome />
-                </Link>
+                </NavLink>
               </Div>
               <Div>
-                <Link
-                  to={`/feed/${userInfo.nickname}`}
-                  style={{
-                    textDecoration: 'none',
-                    color: 'black',
-                  }}
-                >
-                  {userInfo.nickname}님의 피드
-                </Link>
+                <Info>
+                  <div>
+                    <Img src={userInfo.userImage} />
+                  </div>
+                  <NavLink
+                    to={`/feed/${userInfo.nickname}`}
+                    style={{
+                      textDecoration: 'none',
+                      color: '#3d3c3c',
+                    }}
+                  >
+                    {userInfo.nickname}님의 피드
+                  </NavLink>
+                </Info>
               </Div>
-              <Div>
-                <Link
+              <Div Text>
+                <NavLink
                   to="/mypage"
                   style={{
                     textDecoration: 'none',
-                    color: 'black',
+                    color: '#3d3c3c',
                   }}
                 >
                   mypage
-                </Link>
+                </NavLink>
               </Div>
-              <Div>
-                <Link
+              <Div Text>
+                <NavLink
                   onClick={() => props.userInfoToStore()}
                   to="/mainfeed"
                   style={{
                     textDecoration: 'none',
-                    color: 'black',
+                    color: '#3d3c3c',
                   }}
                 >
                   로그아웃
-                </Link>
+                </NavLink>
               </Div>
             </NavList>
           </>
         ) : (
           <>
             <NavList>
-              <Div Home>
-                <Link
+              <Div>
+                <NavLink
                   to="/mainfeed"
                   style={{
                     textDecoration: 'none',
-                    color: 'black',
+                    color: '#3d3c3c',
                     fontSize: '1.7rem',
                   }}
                 >
                   <BiHome />
-                </Link>
+                </NavLink>
               </Div>
-              <Div>
-                <Link
+              <Div Text>
+                <NavLink
                   to="/login"
-                  style={{ textDecoration: 'none', color: 'black' }}
+                  style={{ textDecoration: 'none', color: '#3d3c3c' }}
                 >
                   로그인
-                </Link>
+                </NavLink>
               </Div>
-              <Div>
-                <Link
+              <Div Text>
+                <NavLink
                   to="/signup"
-                  style={{ textDecoration: 'none', color: 'black' }}
+                  style={{ textDecoration: 'none', color: '#3d3c3c' }}
                 >
                   회원가입
-                </Link>
+                </NavLink>
               </Div>
             </NavList>
           </>
