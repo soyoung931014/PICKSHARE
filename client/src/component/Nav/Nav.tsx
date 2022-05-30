cc6aa69af4d2b400fc4e6a39a029cceade6976f9
park_so_young <soyoung931014@gmail.com>
2022-05-30 23:52:03 +0900

    ADD: 반응형, 사이드바 구현 #94

--- client/src/component/Nav/Nav.tsx
+++ client/src/component/Nav/Nav.tsx
@@ -9,17 +9,19 @@ import { connect, useSelector } from 'react-redux';
 import { NavLink, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { BiHome } from 'react-icons/bi';
+import camera from '../../img/camera.jpg';
 import { deleteUserInfo } from '../../redux/actions';
+import { FaBars } from 'react-icons/fa';
+import { useState } from 'react';
 const AWS = require('aws-sdk/dist/aws-sdk-react-native');
 
 const Wrapper = styled.section`
-  //border: solid green 2px;
+  // border: solid green 2px;
   width: 100%;
   height: 10vh;
   display: flex;
   justify-content: flex-end;
   align-items: center;
-  padding: 1rem;
   background: white;
   border-bottom: solid 0.8px #bbbbbb;
   margin-bottom: 1rem;
@@ -28,9 +30,9 @@ const Wrapper = styled.section`
 
 const Logo = styled.section`
   //border: solid red 2px;
-  margin-left: 1rem;
+  margin-left: 1.7rem;
   font-weight: bolder;
-  width: 100%;
+  width: 30%;
   height: 2rem;
   background: linear-gradient(to right, #ee64c7, #8272eb, #d06be0);
   -webkit-background-clip: text;
@@ -39,33 +41,80 @@ const Logo = styled.section`
   :hover {
     cursor: pointer;
   }
+  @media screen and (max-width: 977px) {
+    display: none;
+  }
 `;
-const NavList = styled.section`
+
+const CameraImg = styled.img`
+  display: none;
+  @media screen and (max-width: 977px) {
+    height: 4.4vh;
+    display: block;
+    justify-content: flex-start;
+    margin-left: 1.8rem;
+  }
+`;
+const MenuFabar = styled.a`
+  //border: solid 2px green;
+  display: none;
+  @media screen and (max-width: 977px) {
+    display: block;
+    width: 100%;
+    text-align: end;
+    margin: 0.7rem;
+    font-size: 1rem;
+    padding: 0.6rem;
+    margin-right: 1.7rem;
+    :hover {
+      cursor: pointer;
+    }
+  }
+`;
+const NavList = styled.section<{ Menu?: any }>`
   //border: solid blue 2px;
-  width: 160%;
+  width: 100%;
   height: 10vh;
   display: flex;
   justify-content: flex-end;
   align-items: center;
+  margin-right: 1.5rem;
+  @media screen and (max-width: 977px) {
+    width: 50vw;
+    height: 40vh;
+    display: ${({ Menu }) => {
+      return Menu ? 'flex' : 'none';
+    }};
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: flex-start;
+    background-color: #fffceb;
+    margin-top: 39vh;
+    position: relative;
+    left: 64%;
+    top: 54%;
+    padding-right: 5%;
+  }
 `;
 const Div = styled.div<{ Text?: any }>`
   //border: solid blue 2px;
   margin: 0.7rem;
   font-size: 1rem;
+  font-weight: bolder;
   padding: 0.6rem;
+  padding-top: ${(props) => (props.Text ? '1rem' : null)};
+  padding-bottom: ${(props) => (props.Text ? '1rem' : null)};
   :hover {
     cursor: pointer;
     background: #fee9f7;
     border-radius: 10px;
   }
-  padding-top: ${(props) => (props.Text ? '1rem' : null)};
-  padding-bottom: ${(props) => (props.Text ? '1rem' : null)};
 `;
 
 const Img = styled.img`
+  //border: solid red 2px;
   height: 3.5vh;
   width: 1.8vw;
-  //border: solid red 2px;
   border-radius: 50%;
   padding: 2px;
   margin-right: 0.5rem;
@@ -82,6 +131,8 @@ const Nav = (props: any) => {
   );
   const navigate = useNavigate();
 
+  const [menu, setMenu] = useState(false);
+
   AWS.config.update({
     region: 'us-east-1',
     credentials: new AWS.CognitoIdentityCredentials({
@@ -93,9 +144,13 @@ const Nav = (props: any) => {
     <>
       <Wrapper>
         <Logo onClick={() => navigate('/mainfeed')}>PICHSHARE</Logo>
+        <div>
+          <CameraImg onClick={() => navigate('/mainfeed')} src={camera} />
+        </div>
+
         {isLogin === true ? (
           <>
-            <NavList>
+            <NavList Menu={menu}>
               <Div>
                 <NavLink
                   to="/mainfeed"
@@ -151,7 +206,7 @@ const Nav = (props: any) => {
           </>
         ) : (
           <>
-            <NavList>
+            <NavList Menu={menu}>
               <Div>
                 <NavLink
                   to="/mainfeed"
@@ -183,6 +238,15 @@ const Nav = (props: any) => {
             </NavList>
           </>
         )}
+        <MenuFabar href="#" onClick={() => setMenu(!menu)}>
+          <FaBars
+            style={{
+              fontWeight: 'bolder',
+              fontSize: '3vh',
+              color: '#3d3c3c',
+            }}
+          />
+        </MenuFabar>
       </Wrapper>
     </>
   );
