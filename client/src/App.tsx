fd9761048a253c94a4e3ce512c33c1c81fd4378c
park_so_young <soyoung931014@gmail.com>
2022-06-01 23:16:34 +0900

    ADD: 성능개선: lazy import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,32 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import MyPage from './component/MyPage/MyPage';
-import Login from './component/User/Login/Login';
-import Signup from './component/User/Signup/Signup';
 import GlobalStyles from './GlobalStyles';
-import LandingPage from './pages/LandingPage';
-
-import MainFeed from './pages/MainFeed';
-import UserFeed from './pages/UserFeed';
-import ErrorLoadingPage from './pages/ErrorLoadingPage';
-import KakaoLoading from './pages/KakaoLoading';
+import { lazy, Suspense } from 'react';
+const MyPage = lazy(() => import('./component/MyPage/MyPage'));
+const Login = lazy(() => import('./component/User/Login/Login'));
+const Signup = lazy(() => import('./component/User/Signup/Signup'));
+const LandingPage = lazy(() => import('./pages/LandingPage'));
+const MainFeed = lazy(() => import('./pages/MainFeed'));
+const UserFeed = lazy(() => import('./pages/UserFeed'));
+const ErrorLoadingPage = lazy(() => import('./pages/ErrorLoadingPage'));
+const KakaoLoading = lazy(() => import('./pages/KakaoLoading'));
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyles />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        {/*  <Route path="/diary" element={<DiaryPage />} /> */}
-        <Route path="/mainfeed" element={<MainFeed />} />
-        <Route path="/feed/:nickname" element={<UserFeed />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/loading" element={<KakaoLoading />} />
-        <Route path="/errorloading" element={<ErrorLoadingPage />} />
-      </Routes>
+      <Suspense fallback={<div>로딩중</div>}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          {/*  <Route path="/diary" element={<DiaryPage />} /> */}
+          <Route path="/mainfeed" element={<MainFeed />} />
+          <Route path="/feed/:nickname" element={<UserFeed />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/loading" element={<KakaoLoading />} />
+          <Route path="/errorloading" element={<ErrorLoadingPage />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
