a1e33aa324d13e27501ff15f118bc5935a44216e
park_so_young <soyoung931014@gmail.com>
2022-06-02 11:30:58 +0900

    FIX: main.ts변경

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/PICKSHARE/server
-pm2 stop app.js 2> /dev/null || true
-pm2 delete app.js 2> /dev/null || true
+pm2 stop main.ts 2> /dev/null || true
+pm2 delete main.ts 2> /dev/null || true
