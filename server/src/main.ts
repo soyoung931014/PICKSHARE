d300627d3d264708c10f6ae2d5da86b0784a45bc
park_so_young <soyoung931014@gmail.com>
2022-06-03 22:51:09 +0900

    UPDATE: 수정

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
-  await app.listen(process.env.SERVER_PORT);
+  await app.listen(5000);
   console.log('http server running👊');
 }
 bootstrap();
