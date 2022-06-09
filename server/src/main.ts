ab7be886cb2075b9cfb6bc5271a93a212a260384
park_so_young <soyoung931014@gmail.com>
2022-06-10 00:31:05 +0900

    FIX: PORT이름 변경

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,6 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
