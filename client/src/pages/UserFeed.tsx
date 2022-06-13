6dc9acbb4eafadb80943bedcdbfd39b74aed27cd
Boyoung <boyoung.y589@gmail.com>
2022-06-13 16:36:56 +0900

    FIXED: MERGE bug

--- client/src/pages/UserFeed.tsx
+++ client/src/pages/UserFeed.tsx
@@ -286,7 +286,7 @@ export default function UserFeed() {
                 />
               ))}
         </Feed>
-        <Modal />
+        {/* <Modal /> */}
         <div>
           팔로잉
           {following.id === ''
