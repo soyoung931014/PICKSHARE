d663bd735b5c03f43d1953d67d5a12cb4d2ad6d1
Boyoung <boyoung.y589@gmail.com>
2022-06-13 15:04:26 +0900

    MERGED

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import GlobalStyles from './GlobalStyles';
-import LandingPage from './pages/LandingPage';
-import MainFeed from './pages/MainFeed';
-import UserFeed from './pages/UserFeed';
-import ErrorLoadingPage from './pages/ErrorLoadingPage';
-import KakaoLoading from './pages/KakaoLoading';
-import DiaryPage from './pages/DiaryPage';
 import { lazy, Suspense } from 'react';
 import CommentSection from './component/Comment/Comments';
 
@@ -17,6 +11,7 @@ const MainFeed = lazy(() => import('./pages/MainFeed'));
 const UserFeed = lazy(() => import('./pages/UserFeed'));
 const ErrorLoadingPage = lazy(() => import('./pages/ErrorLoadingPage'));
 const KakaoLoading = lazy(() => import('./pages/KakaoLoading'));
+const DiaryPage = lazy(() => import('./pages/DiaryPage'));
 
 function App() {
   return (
@@ -25,7 +20,7 @@ function App() {
       <Suspense fallback={<div>로딩중</div>}>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          {/*  <Route path="/diary" element={<DiaryPage />} /> */}
+          <Route path="/diary" element={<DiaryPage />} />
           <Route path="/mainfeed" element={<MainFeed />} />
           <Route path="/feed/:nickname" element={<UserFeed />} />
           <Route path="/signup" element={<Signup />} />
