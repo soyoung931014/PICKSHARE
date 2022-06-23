bcb0df4926a2dd5506826e9ec2eb73bc56a9749f
park_so_young <soyoung931014@gmail.com>
2022-06-23 09:43:48 +0900

    Revert "UPDATE: 수정"
    This reverts commit 5986567cc5c64749f0d4af9df64df8e30f4ce489.

--- server/src/module/user/user.service.ts
+++ server/src/module/user/user.service.ts
@@ -1,10 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-var-requires */
-import {
-  Injectable,
-  NotFoundException,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { LoginDto } from './dto/login-user.dto';
 import { SignUpDto } from './dto/singup-user.dto';
@@ -12,6 +8,7 @@ import { UserRepository } from './user.repository';
 import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
 import axios from 'axios';
+import { NotFoundError } from 'rxjs';
 require('dotenv').config();
 
 @Injectable()
@@ -177,8 +174,8 @@ export class UserService {
       nickname: userNickname,
     });
 
-    if (!info) {
-      throw new NotFoundException(`Can't find user nickname ${userNickname}`);
+    if(!info){
+      throw new NotFoundException(`Can't find user nickname ${userNickname}`); 
     }
 
     return {
