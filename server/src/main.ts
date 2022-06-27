3764094b44f710b49f5b4706e69388abd0710072
park_so_young <soyoung931014@gmail.com>
2022-06-28 01:18:17 +0900

    FIX: CORS 수정 #142

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,15 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://www.pickshareapp.com',
+      'http://www.pickshareapp.com',
+      'http://localhost:3000',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
