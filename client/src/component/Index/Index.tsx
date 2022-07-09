ccadc44b57b1ff51c8ca72ff4be38178062d8c93
park_so_young <soyoung931014@gmail.com>
2022-07-09 22:49:30 +0900

    UPDATE: Index 컴포넌트로  분리 #144

---
+++ client/src/component/Index/Index.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import styled from 'styled-components';
+import homeIndex from '../../img/homeIndex.png';
+import signupIndex from '../../img/signupIndex.png';
+import signinIndex from '../../img/signinIndex.png';
+import { useNavigate } from 'react-router-dom';
+
+const BookMark = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 80vh;
+`;
+const TagHome = styled.img`
+  width: 7rem;
+  height: 4.5rem;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.05);
+    cursor: pointer;
+  }
+  @media screen and (max-width: 891px) {
+    display: none;
+  }
+`;
+const TagSignin = styled.img`
+  width: 11rem;
+  height: 4rem;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.05);
+    cursor: pointer;
+  }
+  @media screen and (max-width: 891px) {
+    display: none;
+  }
+`;
+const TagSignup = styled.img`
+  width: 8rem;
+  height: 5rem;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.05);
+    cursor: pointer;
+  }
+  position: relative;
+  top: -10px;
+  @media screen and (max-width: 891px) {
+    display: none;
+  }
+`;
+function Index() {
+  const navigate = useNavigate();
+  return (
+    <BookMark>
+      <TagHome
+        src={homeIndex}
+        onClick={() => {
+          navigate('/mainfeed', { replace: true });
+        }}
+      ></TagHome>
+      <TagSignin
+        src={signinIndex}
+        onClick={() => {
+          navigate('/login', { replace: true });
+        }}
+      ></TagSignin>
+      <TagSignup
+        src={signupIndex}
+        onClick={() => {
+          navigate('/signup', { replace: true });
+        }}
+      ></TagSignup>
+    </BookMark>
+  );
+}
+
+export default Index;
