29c9db11a96d6e13fd9d056374afdf46758d8772
park_so_young <soyoung931014@gmail.com>
2023-05-05 14:06:18 +0900

    UPDATE: 레이아웃수정

--- client/src/layout/Layout.tsx
+++ client/src/layout/Layout.tsx
@@ -1,27 +1,11 @@
-import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
-import Footer from '../component/Footer/Footer';
-import Nav from '../component/Nav/Nav';
 const Layout = () => {
-  const [state, setState] = useState(false);
-  const hi = window.location.pathname;
-
   return (
     <Container>
-      {hi === '/signup' || hi === '/login' || hi === '/mypage' ? null : (
-        <header>
-          <Nav />
-        </header>
-      )}
       <Main>
         <Outlet />
       </Main>
-      {window.location.pathname === '/signup' ||
-      window.location.pathname === '/login' ||
-      window.location.pathname === '/mypage' ? null : (
-        <Footer />
-      )}
     </Container>
   );
 };
@@ -29,6 +13,6 @@ const Layout = () => {
 export default Layout;
 
 const Container = styled.div`
-  background-color: #ccc;
+  background-color: #ffff;
 `;
 const Main = styled.div``;
