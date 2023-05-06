239f2319a43fb8ebb303f35e3a6baaea92ba0205
park_so_young <80194405+soyoung931014@users.noreply.github.com>
2023-05-06 16:58:09 +0900

    ADD: layout (#153)

--- client/src/layout/Layout.tsx
+++ client/src/layout/Layout.tsx
@@ -1,11 +1,19 @@
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
+
+import Nav from '../component/Nav/Nav';
+import Footer from '../component/Footer/Footer';
+
 const Layout = () => {
   return (
     <Container>
+      <Header>
+        <Nav />
+      </Header>
       <Main>
         <Outlet />
       </Main>
+      <Footer />
     </Container>
   );
 };
@@ -14,5 +22,11 @@ export default Layout;
 
 const Container = styled.div`
   background-color: #ffff;
+  overflow-x: hidden;
+  display: flex;
+  flex-direction: column;
+`;
+const Header = styled.div`
+  height: 70px;
 `;
 const Main = styled.div``;
