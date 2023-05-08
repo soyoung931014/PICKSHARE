4efce4035ffeae16d897047f62b5192f9fd6ccd2
park_so_young <soyoung931014@gmail.com>
2023-05-08 09:19:05 +0900

    ADD: 타입지정

--- client/src/types/user.ts
+++ client/src/types/user.ts
@@ -1,16 +1,53 @@
-export interface auth {
-  isLogin: boolean;
-  accessToken?: string;
-  refreshToken?: string;
-  userInfo?: userInfoProps;
+// 토큰 뽑아오는 데이터
+export interface ITokenData {
+  data: {
+    data: { accessToken: string; loginMethod: number };
+    message: string;
+    statusCode: number;
+  };
 }
-export interface userInfoProps {
-  id?: number;
-  email?: string;
-  nickname?: string;
-  userImage?: string;
-  loginMethod?: number;
-  statusMessage?: string;
-  created_at?: string;
-  updated_at?: string;
+// 회원가입
+export interface ISignup {
+  email: string;
+  nickname: string;
+  password: string;
+}
+// 유저정보데이터from 서버
+export interface IUserData {
+  data: {
+    data: { userInfo: IUser };
+    message: string;
+    statusCode: number;
+  };
+}
+export interface IKakaoData {
+  data: {
+    accessToken: string;
+    data: IKakaoUser;
+    message: string;
+    statusCode: number;
+  };
+}
+export interface IUser {
+  id: number;
+  email: string;
+  nickname: string;
+  userImage: string;
+  loginMethod: number;
+  statusMessage: string;
+  created_at: string;
+  updated_at: string;
+}
+export interface IKakaoUser {
+  email: string;
+  loginMethod: number;
+  nickname: string;
+  statusMessage: string;
+  userImage: string;
+}
+export interface IInitial {
+  isLogin: boolean;
+  accessToken: string;
+  userInfo?: IUser | IKakaoUser;
+  refreshToken?: string;
 }
