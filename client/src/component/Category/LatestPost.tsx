1044889e8d8ea47eab95374f59aab725af1d2072
soyoung931014 <soyoung931014@gmail.com>
2023-07-06 15:50:28 +0900

    ADD: 컴포넌트분리

--- client/src/component/Category/LatestPost.tsx
+++ client/src/component/Category/LatestPost.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction, useEffect } from 'react';
+import InfiniteScroll from '../../common/InfiniteScroll/InfiniteScroll';
+import { Feedlist } from '../../types/feedType';
+import MainFeedList from '../Feed/MainFeed/MainFeedList';
 
-const LatestPost = () => {
-  return <div>LatestPost</div>;
+interface LatestPostProps {
+  dataFetch: (io: IntersectionObserver) => boolean | void | (() => void);
+  list: Feedlist[] | null;
+  target: React.RefObject<HTMLDivElement>;
+  setTargetLoading: (par: boolean) => void;
+}
+const LatestPost = ({
+  dataFetch,
+  list,
+  target,
+  setTargetLoading,
+}: LatestPostProps) => {
+  useEffect(() => {
+    return InfiniteScroll(dataFetch, target, setTargetLoading);
+  }, [target]);
+  return (
+    <>
+      {list.map((el) => (
+        <MainFeedList {...el} key={Math.random() * 100} isRender />
+      ))}
+    </>
+  );
 };
 
 export default LatestPost;
